0c5aa3f711eb70aa9887791fc6ec22c90c0a0af9
plasti <113269807+plastikup@users.noreply.github.com>
2025-04-05 01:05:51 -0400

    Co-authored-by: ploki365 <ploki365@users.noreply.github.com> Co-authored-by: Coze-e <Coze-e@users.noreply.github.com>

---
+++ main copy.py
@@ -0,0 +1,52 @@
+import numpy as np
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import math
+
+
+# figure setup
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+ax.set_xlim(0, 1)
+ax.set_xticks([])
+ax.set_ylim(0, 1)
+ax.set_yticks([])
+ax.set_aspect('equal')
+
+# balls setup
+balls_pos = np.array([[0.5, 0.5]])
+balls_vel = np.array([0.01])
+balls_angle = np.array([math.pi / 4])  # int(input())])
+balls_size = np.array([128])
+
+# trails
+trails_pos = np.array([])
+
+# plot setup
+plot = ax.plot(
+    balls_pos[:, 0],
+    balls_pos[:, 1],
+    s=balls_size,
+)
+
+
+# MAIN LOOP
+def update(frame_number):
+    # animate balls
+    balls_pos[:, 0] += math.cos(balls_angle) * balls_vel
+    balls_pos[:, 1] += math.sin(balls_angle) * balls_vel
+    # ball hits borders
+    for i in range(len(balls_pos)):
+        print(abs(0.5 - balls_pos[i][0]))
+        if abs(0.5 - balls_pos[i][0]) < 0.5 - balls_size[i] / 2:
+            print(True)
+    
+    # display balls
+    plot.set_offsets(balls_pos)
+    plot.set_sizes(balls_size)
+    plot.set_facecolors((0, 0, 0, 1))
+
+
+# build the animation!!
+animation = FuncAnimation(fig, update, interval=16)
+plt.show()
--- main.py
+++ main.py
@@ -1,46 +1,74 @@
-import numpy as np
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
+tick = 0
 
 # figure setup
 fig = plt.figure(figsize=(8, 8))
-ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(0, 1)
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], frameon=True)
+ax.set_xlim(0, 100)
 ax.set_xticks([])
-ax.set_ylim(0, 1)
+ax.set_ylim(0, 100)
 ax.set_yticks([])
+ax.set_aspect("equal")
 
-# balls setup
-balls_pos = np.array([[0.5, 0.5]])
-balls_vel = np.array([0.01])
-balls_angle = np.array([math.pi / 4])  # int(input())])
-balls_size = np.array([128])
+# define ball
+ball_radius = 1
+ball_velocity = 1
+ball_angle = math.pi / 2.2
+ball = plt.Circle([90, ball_radius], 0, color="black", fill=True, clip_on=False)
+ax.add_artist(ball)
 
-# trails
-trails_pos = np.array([])
-
-# scatter setup
-scat = ax.scatter(
-    balls_pos[:, 0],
-    balls_pos[:, 1],
-    s=balls_size,
-)
+# define trails
+trails = []
+scat = ax.scatter([], [], s=10)
 
 
-# MAIN LOOP
-def update(frame_number):
-    # animate balls
-    balls_pos[:, 0] += math.cos(balls_angle) * balls_vel
-    balls_pos[:, 1] += math.sin(balls_angle) * balls_vel
-    # print(balls_pos[0, :])
-    # display balls
-    scat.set_offsets(balls_pos)
-    scat.set_sizes(balls_size)
+def init():
+    ball.set_radius(ball_radius)
     scat.set_facecolors((0, 0, 0, 1))
 
 
-# build the animation!!
-animation = FuncAnimation(fig, update, interval=16)
+def animate(_):
+    global ball_angle
+    global tick
+
+    if tick % 4 == 0:
+        trails.append(ball.center)
+        scat.set_offsets(trails)
+
+    # cinematics
+    ball.center = (
+        ball.center[0] + ball_velocity * math.cos(ball_angle),
+        ball.center[1] + ball_velocity * math.sin(ball_angle),
+    )
+
+    # ball hits borders
+    sign = math.cos(ball_angle) * math.sin(ball_angle)
+    print(abs(50 - ball.center[0]))
+    if abs(50 - ball.center[0]) > 50 - ball_radius:
+        # horizontal
+        ball.center = (
+            100 - ball_radius if ball.center[0] > 50 else ball_radius,
+            ball.center[1],
+        )
+        # reflections
+        ball_angle += math.pi / 2 * math.copysign(1, sign)
+    if abs(50 - ball.center[1]) > 50 - ball_radius:
+        # vertical
+        ball.center = (
+            ball.center[0],
+            100 - ball_radius if ball.center[1] > 50 else ball_radius,
+        )
+        # reflections
+        ball_angle += math.pi / -2 * math.copysign(1, sign)
+
+    tick += 1
+
+
+ani = animation.FuncAnimation(
+    fig, animate, np.arange(0.4, 2, 0.1), init_func=init(), interval=16, blit=False
+)
 plt.show()
