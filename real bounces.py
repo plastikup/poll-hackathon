a037794007f722fc273f4ab447f9dc3a121e5e02
plasti <113269807+plastikup@users.noreply.github.com>
2025-04-05 11:36:39 -0400

    COLLISIONS BABYYYY

--- real bounces.py
+++ real bounces.py
@@ -34,23 +34,67 @@ class Ball:
 
 
 sack_of_balls = [
-    Ball(95, 1, 1, 2, math.pi / 2.2),
-    Ball(95, 1, 1, 4, math.pi / 1.5),
-    Ball(0, 0, 25, 2, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
+    Ball(95, 1, 5, 1, math.pi / 2.2),
+    Ball(80, 20, 5, 2, math.pi),
+    Ball(40, 1, 5, 2, math.pi / 1.5),
+    Ball(0, 0, 10, 1, math.pi / 4),
 ]
 
 # define balls trails
 trails = []
 scat = ax.scatter([], [], s=10)
 
-# define borders
-
 
 # calculate the snap position between two lines while snapping with a given radius
 def calc_snap_pos(ball_a, ball_nx, ball_ny, ball_r, wall_a, wall_x, wall_y):
     rc = wall_y - ball_r * math.sqrt(wall_a**2 + 1)
+    wm = wall_a - math.tan(ball_a)
     _x = (wall_a * wall_x - math.tan(ball_a) * ball_nx + ball_ny - rc) / (
-        wall_a - math.tan(ball_a)
+        wm + (0.0001 if wm == 0 else 0)
     )
     _y = wall_a * (_x - wall_x) + rc
     return _x, _y
@@ -72,31 +116,63 @@ def animate(_):
         ball.nx += ball.velocity * math.cos(ball.angle)
         ball.ny += ball.velocity * math.sin(ball.angle)
 
+        # ball hits balls
+        for iball in sack_of_balls:
+            if iball == ball:
+                continue
+            dist = math.sqrt((ball.nx - iball.nx) ** 2 + (ball.ny - iball.ny) ** 2)
+            if dist < ball.radius + iball.radius:
+                multpl = 1.5
+
+                displacement = (ball.radius + iball.radius) - dist
+                angle_to_iball = math.atan2(ball.ny - iball.ny, ball.nx - iball.nx)
+
+                x_displ = displacement / 2 * math.cos(angle_to_iball)
+                y_displ = displacement / 2 * math.sin(angle_to_iball)
+                ball.nx += x_displ
+                ball.ny += y_displ
+                ball.angle = math.atan2(
+                    math.sin(ball.angle) + y_displ * multpl,
+                    math.cos(ball.angle) + x_displ * multpl,
+                )
+
+                ix_displ = displacement / -2 * math.cos(angle_to_iball)
+                iy_displ = displacement / -2 * math.sin(angle_to_iball)
+                iball.nx += ix_displ
+                iball.ny += iy_displ
+                iball.angle = math.atan2(
+                    math.sin(iball.angle) + iy_displ * multpl,
+                    math.cos(iball.angle) + ix_displ * multpl,
+                )
+
         # ball hits borders
-        # sign = math.cos(ball.angle) * math.sin(ball.angle)
         if abs(50 - ball.nx) > 50 - ball.radius:
             # horizontal
             if ball.nx > 50:
                 ball.nx, ball.ny = calc_snap_pos(
                     ball.angle, ball.nx, ball.ny, -ball.radius, 1000, 100, 0
                 )
+                ball.nx = min(ball.nx, 99-ball.radius)
             else:
                 ball.nx, ball.ny = calc_snap_pos(
                     ball.angle, ball.nx, ball.ny, ball.radius, 1000, 0, 0
                 )
+                ball.nx = max(ball.nx, ball.radius+1)
             # real reflections
             ball.angle = math.pi - ball.angle
-            print(ball.angle)
         if abs(50 - ball.ny) > 50 - ball.radius:
             # vertical
             if ball.ny > 50:
                 ball.nx, ball.ny = calc_snap_pos(
                     ball.angle, ball.nx, ball.ny, ball.radius, 0, 0, 100
                 )
+                ball.ny = min(ball.ny, 99-ball.radius)
             else:
                 ball.nx, ball.ny = calc_snap_pos(
                     ball.angle, ball.nx, ball.ny, -ball.radius, 0, 0, 0
                 )
+                ball.ny = max(ball.ny, ball.radius+1)
+
             # real reflections
             ball.angle = -ball.angle
 
@@ -110,6 +186,5 @@ ani = animation.FuncAnimation(
     np.arange(0.4, 2, 0.1),
     init_func=(lambda: scat.set_facecolors((0, 0, 0, 1)))(),
     interval=16,
-    blit=False,
 )
 plt.show()
